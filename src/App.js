8b962757f0598a5f0ee0aa4843d5ba8fc3e39a62
airoscar <oscar.y.chen@gmail.com>
2019-05-10 19:42:11 -0600

    working on dynamic input field, not working yet

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import DynamicInputField from './Components/DynamicInputField/DynamicInputField'
+
 function App() {
+
+
+  const change = (id, newValue)=>{console.log(`${id} changed to ${newValue}`)}
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <DynamicInputField
+        id="input1"
+        onChange={change}/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/Components/DynamicInputField/DynamicInputField.js
@@ -0,0 +1,109 @@
+import React, {Component} from 'react'
+import Autosuggest from 'react-autosuggest'
+import './DynamicInputField.css'
+
+const data = [
+    {
+        name: 'C',
+        year: 1972
+    },
+    {
+        name: 'C#',
+        year: 2000
+    },
+    {
+        name: 'C++',
+        year: 1983
+    },
+    {
+        name: 'Clojure',
+        year: 2007
+    }
+  ]
+  
+  // https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions#Using_Special_Characters
+  function escapeRegexCharacters(str) {
+      console.log("escapeRegexCharacters called")
+    return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+  }
+  
+  function getSuggestions(value) {
+    console.log("getSuggestions called with " + value)
+    const escapedValue = escapeRegexCharacters(value.trim())
+    
+    if (escapedValue === '') {
+      return []
+    }
+  
+    const regex = new RegExp('^' + escapedValue, 'i')
+  
+    return data.filter(language => regex.test(data.name))
+  }
+  
+  function getSuggestionValue(suggestion) {
+    return suggestion.name
+  }
+  
+  function renderSuggestion(suggestion) {
+    console.log("renderSuggestion called with " + suggestion.name)
+    return (
+      <span>{suggestion.name}</span>
+    )
+  }
+  
+  class DynamicInputField extends React.Component {
+    constructor() {
+      super()
+  
+      this.state = {
+        value: '',
+        suggestions: []
+      }  
+    }
+  
+    onChange = (_, { newValue }) => {
+      const { id, onChange } = this.props;
+      
+      this.setState({
+        value: newValue
+      })
+      
+      onChange(id, newValue)
+    }
+    
+    onSuggestionsFetchRequested = ({ value }) => {
+      this.setState({
+        suggestions: getSuggestions(value)
+      })
+    }
+  
+    onSuggestionsClearRequested = () => {
+      this.setState({
+        suggestions: []
+      })
+    }
+  
+    render() {
+      const { id, placeholder } = this.props
+      const { value, suggestions } = this.state
+      const inputProps = {
+        placeholder,
+        value,
+        onChange: this.onChange
+      }
+      
+      return (
+        <Autosuggest 
+          id={id}
+          suggestions={suggestions}
+          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+          onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+          getSuggestionValue={getSuggestionValue}
+          renderSuggestion={renderSuggestion}
+          inputProps={inputProps} 
+        />
+      )
+    }
+  }
+
+  export default DynamicInputField;
