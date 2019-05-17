c24d87c40e41e0a135d81516b71263096c8b55d9
airoscar <oscar.y.chen@gmail.com>
2019-05-17 17:24:45 -0600

    debugged autosuggestion

--- src/Components/DynamicInputField/DynamicInputField.js
+++ src/Components/DynamicInputField/DynamicInputField.js
@@ -1,89 +1,90 @@
-import React from 'react'
-import Autosuggest from 'react-autosuggest'
-import './DynamicInputField.css'
-
+import React from "react";
+import Autosuggest from "react-autosuggest";
+import "./DynamicInputField.css";
 
 class DynamicInputField extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-      value: '',
-      suggestions: [],
-    }
-
+      value: ""
+      // suggestions: []
+    };
   }
 
-  escapeRegexCharacters = (str) => {
-    return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
-  }
+  getSuggestions = value => {
+    return this.props.data;
+    // const escapedValue = this.escapeRegexCharacters(value.trim());
+    // if (escapedValue === "") {
+    //   return [];
+    // }
+    // const regex = new RegExp("^" + escapedValue, "i");
+    // return this.props.data.filter(datum => regex.test(datum.code));
+  };
 
-  getSuggestions = (value) => {
+  getSuggestionValue = suggestion => {
+    return suggestion.code;
+  };
 
-    const escapedValue = this.escapeRegexCharacters(value.trim())
-
-    if (escapedValue === '') {
-      return []
-    }
-
-    const regex = new RegExp('^' + escapedValue, 'i')
-
-    return this.props.data.filter(datum => regex.test(datum.code))
-  }
-
-  getSuggestionValue = (suggestion) => {
-    return suggestion.code
-  }
-
-  renderSuggestion = (suggestion) => {
+  renderSuggestion = suggestion => {
     return (
-      <span>{suggestion.code}</span>
-    )
-  }
+      <span>
+        {suggestion.description === ""
+          ? suggestion.code
+          : suggestion.code + ": " + suggestion.description}
+      </span>
+    );
+  };
 
   onChange = (_, { newValue }) => {
-    const { id, onChange } = this.props;
+    // const { id, onChange } = this.props;
 
     this.setState({
       value: newValue
-    })
+    });
 
-    onChange(id, newValue)
-  }
+    // onChange(id, newValue);
+  };
 
   onSuggestionsFetchRequested = ({ value }) => {
+    const { id, onChange } = this.props;
+    onChange(id, value);
+
     this.setState({
       suggestions: this.getSuggestions(value)
-    })
-  }
+    });
+  };
 
   onSuggestionsClearRequested = () => {
     this.setState({
       suggestions: []
-    })
-  }
+    });
+  };
+
+  escapeRegexCharacters = str => {
+    return str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+  };
 
   render() {
-
-    const { id, placeholder } = this.props
-    const { value, suggestions } = this.state
+    const { id, placeholder } = this.props;
+    const { value, suggestions } = this.state;
     const inputProps = {
-      placeholder,
+      placeholder: "Enter a code",
       value,
       onChange: this.onChange
-    }
+    };
 
     return (
       <Autosuggest
         id={id}
-        suggestions={suggestions}
+        suggestions={this.props.data}
         onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
         onSuggestionsClearRequested={this.onSuggestionsClearRequested}
         getSuggestionValue={this.getSuggestionValue}
         renderSuggestion={this.renderSuggestion}
         inputProps={inputProps}
       />
-    )
+    );
   }
 }
 
