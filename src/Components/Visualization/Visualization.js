4f4b464def747a749b5175860333443f6f6b9e0f
Savith Jayasekera <scjayase@ucalgary.ca>
2019-06-25 10:02:56 -0600

    added default layouts to the visualization page

--- src/Components/Visualization/Visualization.js
+++ src/Components/Visualization/Visualization.js
@@ -8,29 +8,34 @@ import RulesTable from "../RulesTable/RulesTable";
 import "./Visualization.css";
 
 const defaultLayoutLg = [
-  { w: 6, h: 17, x: 0, y: 0, i: "0" },
-  { w: 6, h: 17, x: 6, y: 0, i: "1" },
-  { w: 4, h: 18, x: 0, y: 17, i: "2" }
+  { w: 7, h: 16, x: 0, y: 0, i: "0" },
+  { w: 5, h: 16, x: 7, y: 0, i: "1" },
+  { w: 7, h: 13, x: 0, y: 16, i: "2" },
+  { w: 5, h: 13, x: 7, y: 16, i: "3" }
 ];
 const defaultLayoutMd = [
-  { w: 6, h: 17, x: 0, y: 0, i: "0" },
-  { w: 6, h: 15, x: 0, y: 17, i: "1" },
-  { w: 4, h: 17, x: 6, y: 0, i: "2" }
+  { w: 5, h: 15, x: 0, y: 0, i: "0" },
+  { w: 5, h: 15, x: 5, y: 0, i: "1" },
+  { w: 5, h: 17, x: 0, y: 15, i: "2" },
+  { w: 5, h: 17, x: 5, y: 15, i: "3" }
 ];
 const defaultLayoutSm = [
-  { w: 4, h: 14, x: 0, y: 0, i: "0" },
-  { w: 4, h: 16, x: 0, y: 14, i: "1" },
-  { w: 2, h: 14, x: 4, y: 0, i: "2" }
+  { w: 6, h: 14, x: 0, y: 0, i: "0" },
+  { w: 6, h: 16, x: 0, y: 14, i: "1" },
+  { w: 6, h: 13, x: 0, y: 30, i: "2" },
+  { w: 6, h: 18, x: 0, y: 43, i: "3" }
 ];
 const defaultLayoutXs = [
   { w: 4, h: 14, x: 0, y: 0, i: "0" },
   { w: 4, h: 15, x: 0, y: 14, i: "1" },
-  { w: 4, h: 8, x: 0, y: 29, i: "2" }
+  { w: 4, h: 8, x: 0, y: 29, i: "2" },
+  { w: 4, h: 14, x: 0, y: 37, i: "3" }
 ];
 const defaultLayoutXxs = [
-  { w: 2, h: 12, x: 0, y: 19, i: "0" },
-  { w: 2, h: 9, x: 0, y: 2, i: "1" },
-  { w: 2, h: 8, x: 0, y: 11, i: "2" }
+  { w: 2, h: 12, x: 0, y: 0, i: "0" },
+  { w: 2, h: 9, x: 0, y: 12, i: "1" },
+  { w: 2, h: 12, x: 0, y: 21, i: "2" },
+  { w: 2, h: 13, x: 0, y: 33, i: "3" }
 ];
 
 const defaultLayouts = {
@@ -66,6 +71,7 @@ function Visualization(props) {
     chordDiagramDiv.current.handleResize();
     barChartDiv.current.handleResize();
     SankeyDiagramDiv.current.handleResize();
+    console.log(layouts);
   }
 
   const shakeDiv = isLayoutModifiable ? "shake" : "";
