586e89dce7530de097a362db12a3078dc9966c10
Savith Jayasekera <scjayase@ucalgary.ca>
2019-07-01 14:55:12 -0600

    added redux dev tools to createStore

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import { createStore } from "redux";
 
 import reducer from "./Store/Reducers/selectedCodes"; //TODO: change name to selectedCodesReducer
 
-const store = createStore(reducer);
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const notFound = () => <h1>Not Found</h1>;
 
