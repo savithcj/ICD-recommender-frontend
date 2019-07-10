be0b779411388cd9073079f06ba62742c647a560
Savith Jayasekera <scjayase@ucalgary.ca>
2019-07-10 11:18:21 -0600

    adding docs to ListViewer

--- src/Components/ListViewer/ListViewer.js
+++ src/Components/ListViewer/ListViewer.js
@@ -21,7 +21,6 @@ import { sortableContainer, sortableElement, sortableHandle } from "react-sortab
 import arrayMove from "array-move";
 import Menu from "../ComponentMenu/ComponentMenu";
 import LoadingIndicator from "../LoadingIndicator/LoadingIndicator";
-import "./ListViewer.css";
 
 //theme used by the accept and reject buttons
 const theme = createMuiTheme({
@@ -31,6 +30,7 @@ const theme = createMuiTheme({
   }
 });
 
+//Styles used by the different components used by the ListViewer
 const useStyles = makeStyles(() => ({
   root: {
     height: "100%",
@@ -74,172 +74,231 @@ const useStyles = makeStyles(() => ({
   }
 }));
 
-//Memoized ListViewer
-export default React.memo(
-  function ListViewer(props) {
-    const classes = useStyles();
-    const [areItemsRearrangable, setItemRearrangeMode] = useState(false);
+/**
+ * Component used to display the contents of a array of objects.
+ * Displays the objects as a sortable list of cards
+ */
+// Component Documentation
+// This component utilizes the React HOC concept multiple times; https://reactjs.org/docs/higher-order-components.html
+// HOC is a function that takes a component and returns a new component.
+//
+// Main 3rd party components used:
+// -> https://github.com/clauderic/react-sortable-hoc
+//  * -> https://material-ui.com/components/lists/
+//  * -> https://material-ui.com/components/cards/
+//  *
+//  * Props:
+//  * -> title: use to specify a title to be displayed on the top left of the component
+//  * -> disableTitleGutters: passed down to the MaterialUI List component composed in this component.
+//  *    Removes additional padding around the title; refer to the MaterialUI List API docs for a detailed explanation
+function ListViewer(props) {
+  //Binding the styles defined earlier
+  const classes = useStyles();
+  //The component uses a state to switch back and forth between sortable and locked modes
+  const [areItemsSortable, setSortMode] = useState(false);
 
-    //function that gets called after ListViewer items are rearranged
-    const onSortEnd = ({ oldIndex, newIndex }) => {
-      props.onSortEndCallback(arrayMove(props.items, oldIndex, newIndex));
-    };
+  //function that gets called after ListViewer cards are rearranged
+  //used to reassign the indices of the passed array of objects after the user has rearranged the order of the cards
+  const onSortEnd = ({ oldIndex, newIndex }) => {
+    props.onSortEndCallback(arrayMove(props.items, oldIndex, newIndex));
+  };
 
-    //sortableElement() returns a Component. i.e.: sortableElement is a React higher-order component (HOC)
-    //Whereas a component transforms props into UI, a HOC transforms a component into another component.
-    //https://reactjs.org/docs/higher-order-components.html
-    const SortableItem = sortableElement(({ value, description, id, shouldHideDislikeButton }) => {
-      //sortableHandle() is also an HOC
-      const DragHandleButton = sortableHandle(() => (
-        <span>
-          <DragHandle className={classes.drag} />
-        </span>
-      ));
+  //HOC that returns a draggable handle
+  //Used by the SortableContainer to only allow dragging when clicked on this
+  const DragHandleIcon = sortableHandle(() => (
+    <span>
+      <DragHandle className={classes.drag} />
+    </span>
+  ));
 
-      const showExploreButton = props.exploreButton ? (
-        <IconButton id={id} aria-label="Explore" title="Explore on Tree" onClick={props.exploreButton}>
-          <ExploreIcon />
-        </IconButton>
-      ) : null;
+  //When SortableItem component gets within the SortableContainer, it first creates a component as described
+  //within the arrow function passed to the sortableElement function below. Once this component is created,
+  //the sortable functionality is added and returned as a new sortable component by the HOC .
+  const SortableItem = sortableElement(({ id, value, description }) => {
+    //Each of the following buttons are conditionally rendered only if the corresponding prop has been
+    //passed down to the ListViewer
+    //The titles and the onClick actions are also conditionally rendered depending on whether or not
+    //the corresponding properties of the button objects are defined; if they're not defined, default
+    //values are provided.
+    const showExploreButton = props.exploreButton ? (
+      <IconButton
+        id={id}
+        aria-label={props.exploreButton.title ? props.exploreButton.title : "Explore"}
+        title={props.exploreButton.title ? props.exploreButton.title : "Explore"}
+        //The default value for the onClick action is a function that does nothing
+        onClick={props.exploreButton.onClick ? props.exploreButton.onClick : () => {}}
+        disabled={props.exploreButton.shouldDisable ? props.exploreButton.shouldDisable(id) : false}
+      >
+        <ExploreIcon />
+      </IconButton>
+    ) : null;
 
-      const showDragHandleOrExploreButton = areItemsRearrangable ? <DragHandleButton /> : showExploreButton;
+    const showAcceptButton = props.acceptItemButton ? (
+      <IconButton
+        id={id}
+        edge="end"
+        aria-label={props.acceptItemButton.title ? props.acceptItemButton.title : "Accept"}
+        title={props.acceptItemButton.title ? props.acceptItemButton.title : "Accept"}
+        color="primary"
+        onClick={props.acceptItemButton.onClick ? props.acceptItemButton.onClick : () => {}}
+        disabled={props.shouldHideAcceptButton ? props.shouldHideAcceptButton(id) : false}
+      >
+        <CheckIcon />
+      </IconButton>
+    ) : null;
 
-      const itemIndex = <ListItemText className={classes.listItemIndex}>{id + 1}</ListItemText>;
+    const showRemoveButton = props.removeItemButton ? (
+      <IconButton
+        id={id}
+        edge="end"
+        aria-label={props.removeItemButton.title ? props.removeItemButton.title : "Remove"}
+        title={props.removeItemButton.title ? props.removeItemButton.title : "Remove"}
+        color="secondary"
+        onClick={props.removeItemButton.onClick}
+        disabled={props.shouldHideRemoveButton ? props.shouldHideRemoveButton(id) : false}
+      >
+        <RejectIcon />
+      </IconButton>
+    ) : null;
 
-      const showAcceptButton = props.acceptItemButton ? (
-        <IconButton
-          id={id}
-          edge="end"
-          aria-label={props.acceptItemButton.title}
-          title={props.acceptItemButton.title}
-          color="primary"
-          onClick={props.acceptItemButton.onClick}
-          disabled={props.shouldHideAcceptButton ? props.shouldHideAcceptButton(id) : false}
-        >
-          <CheckIcon />
-        </IconButton>
-      ) : null;
+    const showDislikeButton = props.dislikeButton ? (
+      <IconButton
+        id={id}
+        edge="end"
+        aria-label={props.dislikeButton.title ? props.dislikeButton.title : "Dislike"}
+        title={props.dislikeButton.title ? props.dislikeButton.title : "Dislike"}
+        color="secondary"
+        onClick={props.dislikeButton.onClick ? props.dislikeButton.onClick : () => {}}
+        disabled={props.dislikeButton.shouldDisable ? props.dislikeButton.shouldDisable(id) : false}
+      >
+        <ThumbDownIcon />
+      </IconButton>
+    ) : null;
 
-      const showRemoveButton = props.removeItemButton ? (
-        <IconButton
-          id={id}
-          edge="end"
-          aria-label={props.removeItemButton.title}
-          title={props.removeItemButton.title}
-          color="secondary"
-          onClick={props.removeItemButton.onClick}
-          disabled={props.shouldHideRemoveButton ? props.shouldHideRemoveButton(id) : false}
-        >
-          <RejectIcon />
-        </IconButton>
-      ) : null;
+    //conditionally show either the explore button or the draggable handle depending on whether the
+    //ListViewer is in the sortable mode or not
+    const showDragHandleOrExploreButton = areItemsSortable ? <DragHandleIcon /> : showExploreButton;
 
-      const showDislikeButton = props.dislikeButton ? (
-        <IconButton
-          id={id}
-          edge="end"
-          aria-label={props.dislikeButton.title}
-          title={props.dislikeButton.title}
-          color="secondary"
-          onClick={props.dislikeButton.onClick}
-          disabled={shouldHideDislikeButton}
-        >
-          <ThumbDownIcon />
-        </IconButton>
-      ) : null;
+    //the itemIndex is used to show index of the corresponding array object when the ListViewer is in
+    //the sortable mode
+    const ShowItemIndex = <ListItemText className={classes.listItemIndex}>{id + 1}</ListItemText>;
 
+    return (
+      //This is the final element which the SortableElement HOC wraps around.
+      //The element is a Card containing all the details and interaction buttons corresponding to a
+      //particular object in the objects array passed down to the ListViewer
+      <Card className={classes.card}>
+        <ListItem>
+          {showDragHandleOrExploreButton}
+          <ListItemText primary={value} secondary={description === "" ? "Description N/A" : description} />
+          {areItemsSortable ? ShowItemIndex : null}
+          {areItemsSortable ? null : showAcceptButton}
+          {areItemsSortable ? null : showRemoveButton}
+          {areItemsSortable ? null : showDislikeButton}
+        </ListItem>
+      </Card>
+    );
+  });
+
+  //Function that returns a array containing multiple instances of the SortableItem component.
+  //it is used later on by the SortableContainer to create a SortableItem for each object in the
+  //array of objects to be displayed
+  function createItems(arrayOfItems) {
+    return arrayOfItems.map((value, index) => {
       return (
-        <Card className={classes.card}>
-          <ListItem>
-            {showDragHandleOrExploreButton}
-            <ListItemText primary={value} secondary={description === "" ? "Description N/A" : description} />
-            {areItemsRearrangable ? itemIndex : null}
-            {areItemsRearrangable ? null : showAcceptButton}
-            {areItemsRearrangable ? null : showRemoveButton}
-            {areItemsRearrangable ? null : showDislikeButton}
-          </ListItem>
-        </Card>
+        <SortableItem
+          key={`item-${index}`}
+          index={index}
+          id={index}
+          value={value[props.valueName]}
+          description={value[props.descriptionName]}
+          disabled={!areItemsSortable}
+        />
       );
     });
+  }
 
-    function createItems(arrayOfItems) {
-      return arrayOfItems.map((value, index) => {
-        return (
-          <SortableItem
-            key={`item-${index}`}
-            id={index}
-            index={index}
-            value={value[props.valueName]}
-            description={value[props.descriptionName]}
-            disabled={!areItemsRearrangable}
-            shouldHideDislikeButton={value[props.disableDislikeButtonField]}
-          />
-        );
-      });
+  //When the SortableContainer gets used in the return of the ListViewer, it first creates a
+  //list of SortableItems as described within the arrow function passed to the SortableContainer
+  //function below. Once this list is created, the sortableContainer HOC takes the list, adds
+  //the sortable functionality, then returns a new SortableContainer component
+  const SortableContainer = sortableContainer(() => {
+    let displayItems = null;
+
+    //Depending on the value of the items prop passed to the ListViewer, five different
+    //options are available to be displayed by the component
+    if (props.items === undefined) {
+      displayItems = <Typography variant="body2">No items to display</Typography>;
+    } else if (props.items === null) {
+      displayItems = <Typography variant="body2">{props.nullItemsMessage}</Typography>;
+    } else if (props.items === "LOADING") {
+      displayItems = <LoadingIndicator />;
+    } else if (props.items.length === 0) {
+      displayItems = <Typography variant="body2">{props.noItemsMessage}</Typography>;
+    } else {
+      displayItems = <ul className={classes.ul}>{createItems(props.items)}</ul>;
     }
 
-    //sortableContainer also returns a Component; another React HOC
-    const SortableContainer = sortableContainer(() => {
-      let displayItems = null;
-
-      if (props.items === null || props.items === undefined) {
-        displayItems = <Typography variant="body2">{props.nullItemsMessage}</Typography>;
-      } else if (props.items === "LOADING") {
-        displayItems = <LoadingIndicator />;
-      } else if (props.items.length === 0) {
-        displayItems = <Typography variant="body2">{props.noItemsMessage}</Typography>;
-      } else {
-        displayItems = <ul className={classes.ul}>{createItems(props.items)}</ul>;
+    //If the allowRearrage prop is true, add an item to the menuOptions prop allowing
+    //an user to enable the sortable mode
+    if (props.allowRearrage) {
+      const matchingOptions = props.menuOptions.filter(option => option.menuItemText === "Rearrange Items");
+      if (matchingOptions.length === 0) {
+        const rearrangeItems = () => setSortMode(true);
+        props.menuOptions.push({
+          menuItemOnClick: rearrangeItems,
+          menuItemText: "Rearrange Items"
+        });
       }
+    }
 
-      if (props.allowRearrage) {
-        const matchingOptions = props.menuOptions.filter(option => option.menuItemText === "Rearrange Items");
-        if (matchingOptions.length === 0) {
-          const rearrangeItems = () => setItemRearrangeMode(true);
-          props.menuOptions.push({
-            menuItemOnClick: rearrangeItems,
-            menuItemText: "Rearrange Items"
-          });
-        }
-      }
+    //A customizable button shown at the top right of the component allowing the user to
+    //perform an action as specified by the onClick property of the passed button prop
+    const showButton =
+      props.button && !areItemsSortable ? (
+        <Button className={classes.button} onClick={props.button.onClick} title={props.button.title}>
+          {props.button.text}
+        </Button>
+      ) : null;
 
-      const showRearrangeConfirmationOrMenu = areItemsRearrangable ? (
-        <IconButton className={classes.button} title="Confirm Item Order" onClick={() => setItemRearrangeMode(false)}>
-          <CheckMark />
-        </IconButton>
-      ) : (
-        <Menu menuOptions={props.menuOptions} />
-      );
+    //if in sortable mode, the dropdown menu hides and a confirmation icon is shown on the top right, instead of the
+    //button, which when clicked exits the sortable mode
+    const showRearrangeConfirmationOrMenu = areItemsSortable ? (
+      <IconButton className={classes.button} title="Confirm Item Order" onClick={() => setSortMode(false)}>
+        <CheckMark />
+      </IconButton>
+    ) : (
+      <Menu menuOptions={props.menuOptions} />
+    );
 
-      const showButton =
-        props.button && !areItemsRearrangable ? (
-          <Button className={classes.button} onClick={props.button.onClick} title={props.button.title}>
-            {props.button.text}
-          </Button>
-        ) : null;
+    return (
+      //This is the final list of SortableElements which the SortableContainer wraps around.
+      //If the items prop is a list of objects, this will be a list of SortableElements which
+      //were created by the createItems function. Otherwise, this will be a list with a single
+      //item corresponding to a message determined by the value of the items prop
+      <List dense={true} className={classes.root}>
+        <ListSubheader className={classes.listTitle} disableSticky={false} disableGutters={props.disableTitleGutters}>
+          {props.title}
+          {showRearrangeConfirmationOrMenu}
+          {showButton}
+        </ListSubheader>
+        {displayItems}
+      </List>
+    );
+  });
 
-      return (
-        <List dense={true} className={classes.root}>
-          <ThemeProvider theme={theme}>
-            <ListSubheader
-              className={classes.listTitle}
-              disableSticky={false}
-              disableGutters={props.disableTitleGutters}
-            >
-              {props.title}
-              {showRearrangeConfirmationOrMenu}
-              {showButton}
-            </ListSubheader>
-            {displayItems}
-          </ThemeProvider>
-        </List>
-      );
-    });
+  //This is the actual return of the ListViewer
+  return (
+    //This theme provider sets the colors of the accept and remove buttons according to the
+    //theme defined earlier
+    <ThemeProvider theme={theme}>
+      <SortableContainer onSortEnd={onSortEnd} lockAxis="y" useDragHandle />
+    </ThemeProvider>
+  );
+}
 
-    return <SortableContainer onSortEnd={onSortEnd} lockAxis="y" useDragHandle />;
-  },
-  (prevProps, nextProps) => {
-    // Component will only re-render if the array of items to be displayed (i.e., props.items) changes
-    return nextProps.items === prevProps.items;
-  }
-);
+//Exporting the memoized ListViewer; used to limit redundant re-renders
+export default React.memo(ListViewer, (prevProps, nextProps) => {
+  // Component will only re-render if the array of objects to be displayed (i.e., props.items) changes
+  return nextProps.items === prevProps.items;
+});
