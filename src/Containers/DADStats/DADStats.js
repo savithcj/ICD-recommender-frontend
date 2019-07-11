999d3341c8fe9d9b61946b7c88f1819aa7f72ea4
Savith Jayasekera <scjayase@ucalgary.ca>
2019-07-11 14:22:16 -0600

    WIP

--- src/Containers/DADStats/DADStats.js
+++ src/Containers/DADStats/DADStats.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-script-url */
 
-import React from "react";
+import React, { useEffect } from "react";
 import Link from "@material-ui/core/Link";
 import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
@@ -9,6 +9,7 @@ import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Title from "./Title";
+import * as APIUtility from "../../Util/API";
 
 // Generate Order Data
 function createData(id, date, name, shipTo, paymentMethod, amount) {
@@ -23,7 +24,24 @@ const rows = [
   createData(4, "15 Mar, 2019", "Bruce Springsteen", "Long Branch, NJ", "VISA ⠀•••• 5919", 212.79)
 ];
 
+const statistics = null;
+const topTenCodes = null;
+
 export default function Orders() {
+  useEffect(() => {
+    const url = APIUtility.API.getAPIURL(APIUtility.RULES) + "?format=json";
+
+    fetch(url)
+      .then(response => response.json())
+      .then(results => {
+        results.forEach(ruleObject => {
+          ruleObject.rule = ruleObject.lhs + " \u2192 " + ruleObject.rhs;
+          ruleObject.confidence = ruleObject.confidence.toFixed(4);
+          ruleObject.support = ruleObject.support.toFixed(4);
+        });
+      });
+  }, []);
+
   return (
     <React.Fragment>
       <Title>2004-2015 DAD Statistics</Title>
