08488bc2ef0b39ec50ae46a384e7d4dc6efac089
Alexander Krusina <alexander.krusina@ucalgary.ca>
2019-07-17 14:23:29 -0600

    Changed dagger asterisks to show children of a code if it required a sub-code

--- src/Store/Actions/asyncActions.js
+++ src/Store/Actions/asyncActions.js
@@ -78,46 +78,69 @@ export const fetchDaggerAsterisksAndUpdateCache = codeObjArray => {
       getDaggerAsteriskFor.push(codeObj);
     }
   });
-  return dispatch => {
+  return (dispatch, getState) => {
     const stringOfCodes = getStringFromListOfCodes(getDaggerAsteriskFor);
     if (stringOfCodes !== "") {
       const url = APIUtility.API.getAPIURL(APIUtility.DAGGER_ASTERISK) + stringOfCodes + "/?format=json";
 
       dispatch(setDaggerAsterisk("LOADING"));
-
+      //let selectedCodes = Array.from(getState().selected.selectedCodes);
+      let indicesToRemove = [];
       fetch(url)
         .then(response => response.json())
         .then(results => {
           let promiseList = [];
           results.forEach(result => {
             result.combo = result.dagger + "\u271D " + result.asterisk + "*";
-            if (result.combo.includes("-")) {
-              result.combo += " -- Select a sub-code";
-            }
+            // if (result.combo.includes("-")) {
+            //   result.combo += " -- Select a sub-code";
+            // }
             let url2 = APIUtility.API.getAPIURL(APIUtility.CODE_DESCRIPTION);
             if (codeObjArray.find(codeObject => codeObject.code === result.dagger) === undefined) {
-              if (result.dagger.indexOf("-") >= 0) {
-                url2 += result.dagger.slice(0, -1) + "/?format=json";
-              } else {
-                url2 += result.dagger + "/?format=json";
-              }
+              // if (result.dagger.indexOf("-") >= 0) {
+              //   url2 += result.dagger.slice(0, -1) + "/?format=json";
+              // } else {
+              url2 += result.dagger + "/?format=json";
+              promiseList.push(
+                fetch(url2)
+                  .then(response => response.json())
+                  .then(codeObject => {
+                    result.description = codeObject.code + ": " + codeObject.description;
+                  })
+              );
+              // }
+            } else if (codeObjArray.find(codeObject => codeObject.code === result.asterisk) === undefined) {
+              // if (result.asterisk.indexOf("-") >= 0) {
+              //   url2 += result.asterisk.slice(0, -1) + "/?format=json";
+              // } else {
+              url2 += result.asterisk + "/?format=json";
+              promiseList.push(
+                fetch(url2)
+                  .then(response => response.json())
+                  .then(codeObject => {
+                    result.description = codeObject.code + ": " + codeObject.description;
+                  })
+              );
+              // }
             } else {
-              if (result.asterisk.indexOf("-") >= 0) {
-                url2 += result.asterisk.slice(0, -1) + "/?format=json";
-              } else {
-                url2 += result.asterisk + "/?format=json";
-              }
+              indicesToRemove.push(results.indexOf(result));
             }
-            promiseList.push(
-              fetch(url2)
-                .then(response => response.json())
-                .then(codeObject => {
-                  result.description = codeObject.code + ": " + codeObject.description;
-                })
-            );
+            // promiseList.push(
+            //   fetch(url2)
+            //     .then(response => response.json())
+            //     .then(codeObject => {
+            //       result.description = codeObject.code + ": " + codeObject.description;
+            //     })
+            // );
           });
           Promise.all(promiseList).then(() => {
-            dispatch(setDaggerAsterisk(results));
+            let resultsToSend = [];
+            for (let i = 0; i < results.length; i++) {
+              if (!indicesToRemove.includes(i)) {
+                resultsToSend.push(results[i]);
+              }
+            }
+            dispatch(setDaggerAsterisk(resultsToSend));
           });
         });
     } else {
