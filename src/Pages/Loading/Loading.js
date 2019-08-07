2e5a910f89792a521fc6ca0c5ca2683e99434845
Savith Jayasekera <scjayase@ucalgary.ca>
2019-08-07 14:01:06 -0600

    removed menu bar title from loading page

--- src/Pages/Loading/Loading.js
+++ src/Pages/Loading/Loading.js
@@ -6,7 +6,7 @@ export default function Loading() {
   return (
     // TODO: possibly add a custom stylesheet; just using home css class for now
     <div className="Home">
-      <MenuBar title="ICD-10 Code Suggestion and Usage Insight" hideDropDown />
+      <MenuBar hideDropDown />
       <LoadingIndicator />
     </div>
   );
