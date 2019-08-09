1f48ee22b521f02b7717207ea01d8d835f37fd69
Savith Jayasekera <scjayase@ucalgary.ca>
2019-08-09 14:16:21 -0600

    changed sign up success message

--- src/Pages/SignUpSuccess/SignUpSuccess.js
+++ src/Pages/SignUpSuccess/SignUpSuccess.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function SignUpSuccess() {
   return (
     <React.Fragment>
-      <h1>Account sign up request successful. Waiting for admin review...</h1>
+      <h1>Account sign up request successful.</h1>
       <Link to="/sign-in"> Click to go to sign in page </Link>
     </React.Fragment>
   );
