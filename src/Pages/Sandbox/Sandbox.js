2a13d59794fb07cdab0f9e5ad4c40c9b5c6366e3
alexanderkrusina <alexander.krusina@ucalgary.ca>
2019-11-06 15:08:48 -0700

    file reading

---
+++ src/Pages/Sandbox/Sandbox.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import { connect } from "net";
+import { defaultLayouts } from "./layouts";
+import { getFromLS, saveToLS } from "../../Util/layoutFunctions";
+import { WidthProvider, Responsive } from "react-grid-layout";
+import MenuBar from "../../Containers/MenuBar/MenuBar";
+import SelectFile from "../../Containers/SelectFile/SelectFile";
+
+const ResponsiveReactGridLayout = WidthProvider(Responsive);
+const originalLayouts = getFromLS("manageLayouts", "layouts") || defaultLayouts;
+
+function Sandbox(props) {
+  const [layouts, setLayouts] = useState(originalLayouts);
+  const [isLayoutModifiable, setLayoutModifiable] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const resetLayout = () => {
+    setLayouts(defaultLayouts);
+    saveToLS("manageLayouts", "layouts", defaultLayouts);
+  };
+
+  function handleLayoutModifierButton() {
+    const layoutModifiable = !isLayoutModifiable;
+    setLayoutModifiable(layoutModifiable);
+  }
+
+  function onLayoutChange(layouts) {
+    setLayouts(layouts);
+    saveToLS("manageLayouts", "layouts", layouts);
+  }
+
+  const highlightEditDiv = isLayoutModifiable
+    ? "grid-border edit-border"
+    : "grid-border";
+
+  return (
+    <div>
+      <div>
+        <MenuBar
+          title="Sandbox"
+          homeLink
+          adminLink
+          visualizationLink
+          aboutLink
+          handleLayoutConfirm={() => handleLayoutModifierButton()}
+          handleResetLayout={resetLayout}
+          inModifyMode={isLayoutModifiable}
+        />
+      </div>
+      <ResponsiveReactGridLayout
+        rowHeight={30}
+        draggableCancel="input,textarea"
+        isDraggable={isLayoutModifiable}
+        isResizable={isLayoutModifiable}
+        onLayoutChange={(layout, layouts) => onLayoutChange(layouts)}
+      >
+        <div key="0" className={highlightEditDiv}>
+          <div className="cardContainer">
+            <SelectFile />
+          </div>
+        </div>
+      </ResponsiveReactGridLayout>
+    </div>
+  );
+}
+
+export default Sandbox;
